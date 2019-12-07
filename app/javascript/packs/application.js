742097c6682663a6a6ecbec104e8f04cdd0dbaa0
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-12-07 16:45:27 +0000

    precompilation error fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,14 @@ require ("main")
 require ("map-custom")
 require ("slick-custom")
 
+import "bootstrap"
+import "../stylesheets/application"
 
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
+//import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
