1421fbc34b7714f30ba18d9efa6c50550d6ba35c
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-12-08 04:41:24 +0000

    values added version-1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,19 @@
 class HomeController < ApplicationController
 
 def index
+    @books = Book.all
+    @product = Book.order("RANDOM()").limit(16);
 
+    @product.each do |book| 
+    end
 end
 
 def product
+    @books = Book.all
+    @product = Book.order("RANDOM()").limit(16);
 
+    @product.each do |book| 
+    end
 end
 
 def productdetail
