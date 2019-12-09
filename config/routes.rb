59512ba56e5527a5dc176ea81719af0a50cd0f3e
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-12-09 04:33:27 +0000

    landing page link fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'books#index'
+  root 'home#index'
 
   resources :orders
   resources :users
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/productdetail'
   get 'home/product'
+
+  get 'books/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   
