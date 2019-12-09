ab7f8c1c86e284369a55f552de25d843f671587a
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-12-09 06:14:08 +0000

    models to be edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
   root 'home#index'
 
   resources :orders
   resources :users
   resources :books
+
   
 
   get 'home/index'
   get 'home/productdetail'
   get 'home/product'
+  
 
   get 'books/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20191119173336_create_orders.rb
+++ db/migrate/20191119173336_create_orders.rb
@@ -1,13 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.integer :order_id
-      t.integer :quantity
-      t.float :total_price
-      t.datetime :dateoforder
-      t.integer :user_id
-      t.integer :bookid
-
+      t.integer "order_id"
+      t.datetime "dateoforder"
+      t.integer "user_id"
+      t.integer "bookid"
+      t.integer "quantity"
+      t.float "subtotal"
+      t.float "total_price"
       t.timestamps
     end
   end
